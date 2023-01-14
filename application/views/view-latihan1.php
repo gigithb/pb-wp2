4dc32df4180693edf156b4a2d05acafac65fb36d
gigithb <giyusepmail3@gmail.com>
2023-01-14 18:08:18 +0700

    first commit

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,9 @@
+<html>
+
+<body>
+    Halo Kawan.. Yuk kita belajar wp..<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    Ini hasil dari <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
